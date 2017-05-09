5e5247266ce6e38797bb5991f39a960dfb772a8b
Luke Sanderson <cactuar18@hotmail.com>
2017-05-09 01:01:57 +0100

    Gameplay dump! Movement is pretty close to OG pacman

---
+++ Assets/PacmanMovement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacmanMovement : TileMove
+{
+
+    public enum PowerUp { NONE, GHOST };
+    //public enum Direction { left, right, up, down, none };
+    Vector2 moveVec2;
+    Vector2 actualVec2;
+
+    // speed variable to contorl how fast pacman moves
+    public float speed = 0.4f;
+    // destination varaible, where pacman is going
+    Vector3 dest = Vector3.zero;
+    //  private PowerUp powerup = Pacman.PowerUp.NONE;
+    private int score = 0;
+
+    void Start()
+    {
+        // set the destination to the current position (starting position) of pacman
+        dest = transform.position;
+
+    }
+
+    void Update() 
+    {
+        //Input
+
+        actualVec2 = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        moveTo(dest, speed);
+
+        // recheck the keys for a new movement
+        if (transform.position == dest)
+        {
+            if (this.isValidMove(moveVec2))
+            {
+                dest = this.transform.position + (Vector3)moveVec2;
+            }
+        }
+        // Gets the offset from the destination to the current postion (change in direction)
+        Vector2 dir = dest - this.transform.position;
+
+
+
+        if (Input.GetKey(KeyCode.RightArrow) && isValidMove(Vector2.right))
+        {
+            moveVec2 = new Vector2(1, 0);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow) && isValidMove(-Vector2.right))
+        {
+            moveVec2 = new Vector2(-1, 0);
+
+        }
+        else if (Input.GetKey(KeyCode.UpArrow) && isValidMove(Vector2.up))
+        {
+            moveVec2 = new Vector2(0, 1);
+
+        }
+        else if (Input.GetKey(KeyCode.DownArrow)&& isValidMove(-Vector2.up))
+        {
+            moveVec2 = new Vector2(0, -1);
+
+        }
+
+    }
+}
---
+++ Assets/TileMove.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMove : MonoBehaviour {
+
+    Vector2 nextTile;
+    Vector2 curDir;
+
+    void FixedUpdate()
+    {
+        if(nextTile != (Vector2)transform.position)
+        {
+            curDir = nextTile - (Vector2)transform.position;
+            if(curDir != Vector2.zero)
+            {
+                //nothing
+            }
+        }
+    }
+
+    Vector2 getNextTile()
+    {
+
+        return nextTile;
+    }
+    void setNextTile(Vector2 tile)
+    {
+        nextTile = tile;
+    }
+
+    Vector2 getDirNormal()
+    {
+        return curDir.normalized;
+    }
+
+    public bool isValidMove(Vector2 dir)
+    {
+
+        return checkDirCollision(transform.position, dir);
+    }
+
+    bool checkDirCollision(Vector2 pos, Vector2 dir)
+    {
+        return checkCollision(pos + dir);
+    }
+
+    public bool moveToDir(Vector2 dir, float speed)
+    {
+        if (isAt((Vector2)transform.position + (Vector2)dir)) return true;
+        if (this.isValidMove(dir))
+        {
+            // This will smoothly move pacman to its destination, based on speed
+            Vector2 p = Vector2.MoveTowards(
+                (Vector2)transform.position, (Vector2)transform.position + dir, speed
+            );
+            // actually move to the calcualted vector
+            this.GetComponent<Rigidbody2D>().MovePosition(p);
+            return true;
+        }
+        return false;
+
+    }
+
+    public bool checkCollision(Vector2 target)
+    {
+        int layerMask = (1 << 0);
+
+        RaycastHit2D hit = Physics2D.Linecast(target, (Vector2)transform.position, layerMask);
+        return (hit.collider == null);
+    }
+
+    bool isAt(Vector2 target)
+    {
+        return (Vector2)transform.position == target;
+    }
+
+    public bool moveTo(Vector3 target, float speed)
+    {
+        if (this.checkCollision(target))
+        {
+            return this.moveTo_do(target, speed);
+        }
+        return false;
+    }
+
+    public bool moveTo_do(Vector2 target, float speed)
+    {
+        if (!this.isAt(target))
+        {
+            // This will smoothly move pacman to its destination, based on speed
+            Vector2 p = Vector2.MoveTowards(this.transform.position, target, speed);
+            // actually move to the calcualted vector
+            this.GetComponent<Rigidbody2D>().MovePosition(p);
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
