77c4eb1ca6b2cdb536960d5c9a7f67e2d5c23822
Matthew Duddington <mdudd001@gold.ac.uk>
2017-05-10 23:04:44 +0100

    Learning controller now passes it's results through to the movement class

--- AutoPacMan/Assets/Scripts/PacManLearningController.cs
+++ AutoPacMan/Assets/Scripts/PacManLearningController.cs
@@ -7,28 +7,41 @@ public class PacManLearningController : UnitController {
   IBlackBox box;
 
   public PerceptionMap myPerceptionMap;
+  public PacmanMovement myPacmanMovement;
   public int windowWidth = 7;
   public int windowHeight = 7;
 
   // Use FixedUpdate to enable time jumping
   void FixedUpdate() {
     if (IsRunning) {
+      ISignalArray inputArray = box.InputSignalArray;  // Number of inputs is defined in the Optimizer class
+
       Vector2 myGridPosition = new Vector2 (0, 0);  // TODO actually get correct position
-
-      ISignalArray inputArray = box.InputSignalArray;
-
+      
       // Check a window of tiles around PacMan and set the percieved object into the inputs
       for (int x = 0; x < windowWidth; x++) {
         for (int y = 0; y < windowHeight; y++) {
           int gridPositionX = (int)(x + myGridPosition.x - ((windowWidth - 1) * 0.5f));
           int gridPositionY = (int)(y + myGridPosition.y - ((windowHeight - 1) * 0.5f));
-          inputArray[x + (y * windowWidth)] = myPerceptionMap.ReadMap(gridPositionX, gridPositionY);
+          inputArray [x + (y * windowWidth)] = myPerceptionMap.ReadMap (gridPositionX, gridPositionY);
         }
       }
 
-      box.Activate();
+      box.Activate ();
 
-      ISignalArray outputArray = box.OutputSignalArray;
+      ISignalArray outputArray = box.OutputSignalArray;  // Number of outputs is defined in the Optimizer class
+
+      if (outputArray [4] > outputArray [0] && outputArray [4] > outputArray [1] && outputArray [4] > outputArray [2] && outputArray [4] > outputArray [3]) {
+        // myPacmanMovement.SetAiInputVector(new Vector4 (0,0,0,0);  // Don't need to turn this on, as it always resets anyway?
+      } else if (outputArray [0] > outputArray [1] && outputArray [0] > outputArray [2] && outputArray [0] > outputArray [3]) {
+        myPacmanMovement.SetAiInputVector (new Vector4 (1, 0, 0, 0));  // Signal right
+      } else if (outputArray [1] > outputArray [2] && outputArray [1] > outputArray [3]) {
+        myPacmanMovement.SetAiInputVector (new Vector4 (0, 1, 0, 0));  // Signal left
+      } else if (outputArray [2] > outputArray [3]) {
+        myPacmanMovement.SetAiInputVector (new Vector4 (0, 0, 1, 0));  // Signal up
+      } else {
+        myPacmanMovement.SetAiInputVector (new Vector4 (0, 0, 0, 1));  // Signal down
+      }
     }
   }
 
